44f59ac44652cec0cb7237d0ca2242b255747882
dimple1102 <32271472+dimple1102@users.noreply.github.com>
2017-09-29 13:37:57 +0530

    edited-login
    complete login and registration system

--- src/Login/Form.java
+++ src/Login/Form.java
@@ -13,7 +13,7 @@ public class Form extends JFrame implements ActionListener {
 	JButton b, s;
 
 	Form() {
-		
+
 		l3 = new JLabel("Login System");
 		l3.setForeground(Color.darkGray);
 		Font font = new Font("SansSerif", Font.ITALIC, 30);
@@ -83,7 +83,7 @@ public class Form extends JFrame implements ActionListener {
 					}
 					if (value1.equals(user1) && value2.equals(pass1)) {
 						JOptionPane.showMessageDialog(this, "Successful! ");
-						dispose();
+						this.dispose();
 						new Welcome(user1);
 					} else
 						JOptionPane.showMessageDialog(this, "Incorrect username or password", "Error",
@@ -94,7 +94,7 @@ public class Form extends JFrame implements ActionListener {
 					JOptionPane.showMessageDialog(this, "username or password cannot be empty", "Error",
 							JOptionPane.ERROR_MESSAGE);
 				} else {
-					dispose();
+					this.dispose();
 					new Entry(value1, value2, dbUrl, user, pass);
 				}
 			}
@@ -139,6 +139,7 @@ class Welcome {
 		b.addActionListener(new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent e) {
+				f1.dispose();
 
 			}
 		});
@@ -165,12 +166,67 @@ class Entry extends Form {
 		l2.setForeground(Color.blue);
 		JLabel l3 = new JLabel("Phone no.:");
 		l3.setForeground(Color.blue);
+		JLabel l6 = new JLabel("DOB:");
+		l6.setForeground(Color.blue);
+		JLabel l7 = new JLabel("Country:");
+		l7.setForeground(Color.blue);
+
+		JTextField t4 = new JTextField();
+		t4.addKeyListener(new KeyAdapter() {
+			@Override
+			public void keyTyped(KeyEvent e) {
+				if (!Character.isAlphabetic(e.getKeyChar()) && (e.getKeyChar() != '\b') && (e.getKeyChar() != ' ')) {
+					Toolkit.getDefaultToolkit().beep();
+					e.consume();
+					JOptionPane.showMessageDialog(f2, "Characters only");
+				}
+			}
+		});
+
+		JComboBox<String> cb = new JComboBox<>();
+		cb.addItem("Year");
+		for (int i = 2016; i > 1900; i--) {
+			cb.addItem(Integer.toString(i));
+		}
+
+		JComboBox<String> cb1 = new JComboBox<>();
+		cb1.addItem("Month");
+		for (int i = 01; i <= 12; i++) {
+			cb1.addItem(Integer.toString(i));
+		}
+
+		JComboBox<String> cb2 = new JComboBox<>();
+		cb2.addItem("Date");
+		for (int i = 01; i <= 31; i++) {
+			cb2.addItem(Integer.toString(i));
+		}
+
 		JLabel l5 = new JLabel("Password:");
 		l5.setForeground(Color.blue);
 		JLabel l4 = new JLabel("Confirm Password:");
 		l4.setForeground(Color.blue);
 		JTextField t1 = new JTextField();
+		t1.addKeyListener(new KeyAdapter() {
+			@Override
+			public void keyTyped(KeyEvent e) {
+				if (!Character.isAlphabetic(e.getKeyChar()) && (e.getKeyChar() != '\b') && (e.getKeyChar() != ' ')) {
+					Toolkit.getDefaultToolkit().beep();
+					e.consume();
+					JOptionPane.showMessageDialog(f2, "Characters only");
+				}
+			}
+		});
 		JTextField t2 = new JTextField();
+		t2.addKeyListener(new KeyAdapter() {
+			@Override
+			public void keyTyped(KeyEvent e) {
+				if (!Character.isAlphabetic(e.getKeyChar()) && (e.getKeyChar() != '\b') && (e.getKeyChar() != ' ')) {
+					Toolkit.getDefaultToolkit().beep();
+					e.consume();
+					JOptionPane.showMessageDialog(f2, "Characters only");
+				}
+			}
+		});
 		JTextField t3 = new JTextField();
 		t3.addKeyListener(new KeyAdapter() {
 
@@ -187,16 +243,22 @@ class Entry extends Form {
 		JPasswordField p = new JPasswordField();
 		JPasswordField p1 = new JPasswordField();
 		l.setBounds(75, 20, 250, 40);
-		l1.setBounds(75, 100, 100, 20);
-		t1.setBounds(210, 100, 130, 20);
-		l2.setBounds(75, 200, 100, 20);
-		t2.setBounds(210, 200, 130, 20);
-		l3.setBounds(75, 300, 100, 20);
-		t3.setBounds(210, 300, 130, 20);
-		l5.setBounds(75, 400, 125, 20);
-		p1.setBounds(210, 400, 130, 20);
-		l4.setBounds(75, 500, 125, 20);
-		p.setBounds(210, 500, 130, 20);
+		l1.setBounds(75, 70, 100, 20);
+		t1.setBounds(210, 70, 130, 20);
+		l2.setBounds(75, 140, 100, 20);
+		t2.setBounds(210, 140, 130, 20);
+		l3.setBounds(75, 210, 100, 20);
+		t3.setBounds(210, 210, 130, 20);
+		l6.setBounds(75, 280, 100, 20);
+		cb.setBounds(210, 280, 70, 20);
+		cb1.setBounds(280, 280, 70, 20);
+		cb2.setBounds(350, 280, 70, 20);
+		l7.setBounds(75, 350, 130, 20);
+		t4.setBounds(210, 350, 130, 20);
+		l5.setBounds(75, 420, 125, 20);
+		p1.setBounds(210, 420, 130, 20);
+		l4.setBounds(75, 490, 125, 20);
+		p.setBounds(210, 490, 130, 20);
 		JButton b = new JButton("Submit");
 		b.setBounds(140, 600, 80, 20);
 		b.setBackground(Color.cyan);
@@ -206,30 +268,58 @@ class Entry extends Form {
 				String s1 = t1.getText();
 				String s2 = t2.getText();
 				String s3 = t3.getText();
+				int len = s3.length();
+				String s6 = t4.getText();
 				String s4 = new String(p.getPassword());
 				String s5 = new String(p1.getPassword());
-				if (s5.equals(s4)) {
-					try {
-						Connection con = DriverManager.getConnection(dbUrl, user, pass);
-						PreparedStatement ps = con.prepareStatement("insert into login values(?,?,?,?,?)");
-						ps.setString(1, value1);
-						ps.setString(2, s4);
-						ps.setString(3, s1);
-						ps.setString(4, s2);
-						ps.setString(5, s3);
-						ps.executeUpdate();
-						String msg = "congrats! you are a registered user";
-						JOptionPane.showMessageDialog(f2, msg, "Successful", JOptionPane.PLAIN_MESSAGE);
-						f2.dispose();
-						new Welcome(value1);
-					} catch (SQLException e1) {
-						e1.printStackTrace();
-					}
+				String year = cb.getSelectedItem().toString();
+				String month = cb1.getSelectedItem().toString();
+				String date = cb2.getSelectedItem().toString();
+				if (year.equals("Year") || month.equals("Month") || date.equals("Date")) {
+					JOptionPane.showMessageDialog(f2, "Invalid DOB!", "Error", JOptionPane.ERROR_MESSAGE);
 				} else {
-					JOptionPane.showMessageDialog(f2, "Password doesn't match", "Error", JOptionPane.ERROR_MESSAGE);
+					String d = year + "-" + month + "-" + date;
+					if (len != 10) {
+						JOptionPane.showMessageDialog(f2, "Invalid Phone Number!", "Error", JOptionPane.ERROR_MESSAGE);
+					} else {
+						if (s6.equals("")) {
+							JOptionPane.showMessageDialog(f2, "Invalid Country!", "Error", JOptionPane.ERROR_MESSAGE);
+						} else {
+							if (s5.equals(s4)) {
+								try {
+									Connection con = DriverManager.getConnection(dbUrl, user, pass);
+									PreparedStatement ps = con
+											.prepareStatement("insert into login values(?,?,?,?,?,?,?)");
+									ps.setString(1, value1);
+									ps.setString(2, s4);
+									ps.setString(3, s1);
+									ps.setString(4, s2);
+									ps.setString(5, s3);
+									ps.setString(6, d);
+									ps.setString(7, s6);
+									ps.executeUpdate();
+									String msg = "congrats! you are a registered user";
+									JOptionPane.showMessageDialog(f2, msg, "Successful", JOptionPane.PLAIN_MESSAGE);
+									f2.dispose();
+									new Welcome(value1);
+								} catch (SQLException e1) {
+									e1.printStackTrace();
+								}
+							} else {
+								JOptionPane.showMessageDialog(f2, "Password doesn't match", "Error",
+										JOptionPane.ERROR_MESSAGE);
+							}
+						}
+					}
 				}
 			}
 		});
+		f2.add(l7);
+		f2.add(t4);
+		f2.add(cb2);
+		f2.add(cb1);
+		f2.add(cb);
+		f2.add(l6);
 		f2.add(l);
 		f2.add(p);
 		f2.add(p1);
